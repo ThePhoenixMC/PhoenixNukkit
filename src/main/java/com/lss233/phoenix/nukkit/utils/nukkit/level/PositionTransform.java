c6bdc530bac9eadb251b13d864a4f94adf70222f
lss233 <ls1102086624@outlook.com>
2018-07-14 13:04:55 +0800

    Add PositionTransform.
    Signed-off-by: lss233 <ls1102086624@outlook.com>

---
+++ src/main/java/com/lss233/phoenix/nukkit/utils/nukkit/level/PositionTransform.java
@@ -0,0 +1,18 @@
+package com.lss233.phoenix.nukkit.utils.nukkit.level;
+
+import cn.nukkit.level.Location;
+import cn.nukkit.level.Position;
+import com.lss233.phoenix.math.Vector;
+
+/**
+ *
+ */
+public interface PositionTransform {
+    default Position toNukkit(Vector location){
+        return new Position(location.getX(), location.getY(), location.getZ());
+    }
+
+    default Vector toPhoenix(Position location){
+        return new Vector(location.getX(), location.getY(), location.getZ());
+    }
+}
